aabd89a1028a1d76acf035b229aa3db07eca5299
yuu5322 <yukah5322@gmail.com>
2021-12-09 19:41:29 +0900

    ページ送りする（先生にやってもらった）

--- get_html.py
+++ get_html.py
@@ -1,14 +1,16 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 
 for page in range(1,11):
    r = requests.get("https://maquia.hpplus.jp/makeup/news/?page="+str(page))
+   time.sleep(1)
    r.encoding = r.apparent_encoding
    response = r.text
 
-soup = BeautifulSoup(response,'lxml')
-articles = soup.findAll('div', class_='article-card-media')
+   soup = BeautifulSoup(response,'lxml')
+   articles = soup.findAll('div', class_='article-card-media')
 
-for article in articles:
-   link = article.a.get('href')
-   print(link)
+   for article in articles:
+      link = article.a.get('href')
+      print(link)
