dcd3dfa7368fed0ce6e6f511657faba5256813f9
yuu5322 <yukah5322@gmail.com>
2021-12-09 19:21:43 +0900

    前のコード復元

--- get_articles_html.py
+++ get_articles_html.py
@@ -1,12 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-for page in range(1,11):
-   r = requests.get("https://maquia.hpplus.jp/makeup/news/?page="+str(page))
-   r.encoding = r.apparent_encoding
-   response = r.text
-
-soup = BeautifulSoup(response,'lxml')
+response = requests.get('https://maquia.hpplus.jp/makeup/news/')
+soup = BeautifulSoup(response.text,'lxml')
 articles = soup.findAll('div', class_='article-card-media')
 
 for article in articles:
