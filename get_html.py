b41e5115109e03f0705dbf78cfe6fd150e4e10a9
yuu5322 <yukah5322@gmail.com>
2021-12-09 18:45:07 +0900

    get links of articles

--- get_html.py
+++ get_html.py
@@ -4,5 +4,10 @@ from bs4 import BeautifulSoup
 response = requests.get('https://maquia.hpplus.jp/makeup/news/')
 soup = BeautifulSoup(response.text,'lxml')
 articles = soup.findAll('div', class_='article-card-media')
+
+# for article in articles
+#    print(article)
+
 for article in articles:
-   print(article)
+   link = article.a.get('href')
+   print(link)
