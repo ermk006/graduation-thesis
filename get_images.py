2461268f45bc612a7bc0f5566559c1c4259d0236
yuu5322 <yukah5322@gmail.com>
2021-12-14 19:15:46 +0900

    画像を自動でダウンロードできるようにする

--- get_images.py
+++ get_images.py
@@ -1,11 +1,16 @@
 import requests
 import re
+import uuid
 from bs4 import BeautifulSoup
+import time
 
-url = "https://search.nifty.com/imagesearch/search?select=1&q=%s&ss=up"
-keyword = "猫"
-r = requests.get(url%(keyword))
+url = "https://maquia.hpplus.jp/makeup/news/65455/"
+r = requests.get(url)
 soup = BeautifulSoup(r.text,'lxml')
-imgs = soup.find_all('img',src=re.compile('^https://msp.c.yimg.jp/yjimage'))
+imgs = soup.find_all('img',src=re.compile('^https://img-maquia.hpplus.jp/common/large/image/'))
 for img in imgs:
         print(img['src'])
+        r = requests.get(img['src'])
+        time.sleep(1)
+        with open(str('./picture/')+str(uuid.uuid4())+str('.jpeg'),'wb') as file:
+                file.write(r.content)
